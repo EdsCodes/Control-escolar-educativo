e7386e0f9f95764800fe6ac1870544c777305baa
edscodes <edscodes@gmail.com>
2024-07-31 23:03:31 -0500

    cambios menores

--- src/app/features/dashboard/courses/courses.component.ts
+++ src/app/features/dashboard/courses/courses.component.ts
@@ -16,11 +16,11 @@ export class CoursesComponent {
   nextId: number = 6;
 
   coursesDataSource: courses[] = [
-    { idCurso: '1', nombreCurso: 'React', fechaInicioCurso: '08-15-2024', fechaFinCurso: '01-20-2026'},
-    { idCurso: '2', nombreCurso: 'Angular', fechaInicioCurso: '09-15-2024', fechaFinCurso: '02-20-2026'},
-    { idCurso: '3', nombreCurso: 'Js', fechaInicioCurso: '10-15-2024', fechaFinCurso: '03-20-2026'},
-    { idCurso: '4', nombreCurso: 'CSS', fechaInicioCurso: '11-15-2024', fechaFinCurso: '04-20-2026'},
-    { idCurso: '5', nombreCurso: 'HTML5', fechaInicioCurso: '12-15-2024', fechaFinCurso: '05-20-2026'},
+    { idCurso: '1', nombreCurso: 'React', fechaInicioCurso: '08-15-2024', fechaFinCurso: '01-20-2025'},
+    { idCurso: '2', nombreCurso: 'Angular', fechaInicioCurso: '09-15-2024', fechaFinCurso: '02-20-2025'},
+    { idCurso: '3', nombreCurso: 'Js', fechaInicioCurso: '10-15-2024', fechaFinCurso: '03-20-2025'},
+    { idCurso: '4', nombreCurso: 'CSS', fechaInicioCurso: '11-15-2024', fechaFinCurso: '04-20-2025'},
+    { idCurso: '5', nombreCurso: 'SQL', fechaInicioCurso: '12-15-2024', fechaFinCurso: '05-20-2025'},
   ]
 
   constructor(private matDialog: MatDialog) {}
